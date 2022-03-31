9a7ed90319e737f371ee8824cc843a5dfdccb1a6
Pkow <pkow1999@gmail.com>
2022-03-31 20:08:22 +0200

    Atrybuty sie juz nie buguja i licza sie prawidlowo

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,20 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
     connect(ui->Str,&QButtonGroup::buttonClicked,this,&MainWindow::func);
     connect(ui->Dex,&QButtonGroup::buttonClicked,this,&MainWindow::func);
     connect(ui->Sta,&QButtonGroup::buttonClicked,this,&MainWindow::func);
+
+    connect(ui->Chr,&QButtonGroup::buttonClicked,this,&MainWindow::func);
+    connect(ui->Com,&QButtonGroup::buttonClicked,this,&MainWindow::func);
+    connect(ui->Man,&QButtonGroup::buttonClicked,this,&MainWindow::func);
+
+    connect(ui->Int,&QButtonGroup::buttonClicked,this,&MainWindow::func);
+    connect(ui->Wit,&QButtonGroup::buttonClicked,this,&MainWindow::func);
+    connect(ui->Res,&QButtonGroup::buttonClicked,this,&MainWindow::func);
+
+
 }
 MainWindow::~MainWindow()
 {
@@ -41,10 +52,64 @@ void MainWindow::func(QAbstractButton *bt)
         for(int i = del; i < bt->group()->buttons().size(); i++)
         {
             bt->group()->buttons().at(i)->setChecked(false);
-            qDebug() << i;
         }
-        qDebug() << "END";
     }
 }
+int MainWindow::countDots(QButtonGroup *grp)
+{
+    int counter = 0;
+    for(int i = 0; i < grp->buttons().size(); i++)
+    {
+        if(grp->buttons().at(i)->isChecked())
+        {
+           counter++;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return counter;
+}
+QLayout* findParentLayout(QWidget* w, QLayout* topLevelLayout)
+{
+  for (QObject* qo: topLevelLayout->children())
+  {
+     QLayout* layout = qobject_cast<QLayout*>(qo);
+     if (layout != nullptr)
+     {
+        if (layout->indexOf(w) > -1)
+          return layout;
+        else if (!layout->children().isEmpty())
+        {
+          layout = findParentLayout(w, layout);
+          if (layout != nullptr)
+            return layout;
+        }
+     }
+  }
+  return nullptr;
+}
 
+QLayout* findParentLayout(QWidget* w)
+{
+    if (w->parentWidget() != nullptr)
+        if (w->parentWidget()->layout() != nullptr)
+            return findParentLayout(w, w->parentWidget()->layout());
+    return nullptr;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    int counter = 0;
+    for(int i = 0; i < ui->buttonGroup->buttons().size(); i++)
+    {
+        if(ui->buttonGroup->buttons().at(i)->isChecked())
+        {
+            QAbstractButton * bt = static_cast<QAbstractButton *>(findParentLayout(ui->buttonGroup->buttons().at(i)->focusWidget())->layout()->itemAt(1)->layout()->itemAt(0)->widget());
+            qDebug() << countDots(bt->group());
+        }
+    }
+}
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QAbstractButton>
+#include <QList>
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -11,14 +12,15 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
 private slots:
     void func(QAbstractButton *bt);
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    int countDots(QButtonGroup *grp);
 };
 #endif // MAINWINDOW_H
